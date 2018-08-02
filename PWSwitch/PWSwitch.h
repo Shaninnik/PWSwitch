629bcf23ca7b32c233480361cba32ceac3880f3c
Nikita Shanin <shaninnik@gmail.com>
2018-08-02 12:02:50 -0400

    Reorganize project structure

---
+++ PWSwitch/PWSwitch.h
@@ -0,0 +1,19 @@
+//
+//  PWSwitch.h
+//  PWSwitch
+//
+//  Created by Nikita Shanin on 02/08/2018.
+//  Copyright © 2018 Nikita Shanin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PWSwitch.
+FOUNDATION_EXPORT double PWSwitchVersionNumber;
+
+//! Project version string for PWSwitch.
+FOUNDATION_EXPORT const unsigned char PWSwitchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PWSwitch/PublicHeader.h>
+
+
